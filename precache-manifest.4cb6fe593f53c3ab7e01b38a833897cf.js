ff9349b4743060e15d654a8e0cf1983671f8f644
Wenhao Wu <wenhaow@hotmail.com>
2019-10-26 21:57:32 -0700

    Updates

---
+++ precache-manifest.4cb6fe593f53c3ab7e01b38a833897cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b8b3b30a87b5f6b484373a79970904",
+    "url": "/React-Clicky-Game/index.html"
+  },
+  {
+    "revision": "096e9d2dd31b45bfd45b",
+    "url": "/React-Clicky-Game/static/css/main.5e0143b0.chunk.css"
+  },
+  {
+    "revision": "cc92f5c9c93db6e0d88d",
+    "url": "/React-Clicky-Game/static/js/2.5bf8800a.chunk.js"
+  },
+  {
+    "revision": "096e9d2dd31b45bfd45b",
+    "url": "/React-Clicky-Game/static/js/main.c1ea2576.chunk.js"
+  },
+  {
+    "revision": "b63984a5fcbcf7591fe0",
+    "url": "/React-Clicky-Game/static/js/runtime-main.30221162.js"
+  }
+]);
